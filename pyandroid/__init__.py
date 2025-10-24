d0da81b92deafa8872f081471a38fdc311787e97
Subhobhai <sarkarsubhadip604@gmail.com>
2025-10-24 13:53:36 +0530

    Initialize PyAndroid library with core modules

---
+++ pyandroid/__init__.py
@@ -0,0 +1,26 @@
+"""PyAndroid - A Python library for Android application development.
+
+This library provides a Pythonic interface for building Android applications
+using cross-platform development techniques.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Subhobhai"
+__email__ = "subhobhai943@example.com"
+__license__ = "MIT"
+
+from .core import AndroidApp, Activity, Intent
+from .ui import View, Layout, Widget
+from .utils import Logger, FileManager, NetworkManager
+
+__all__ = [
+    'AndroidApp',
+    'Activity', 
+    'Intent',
+    'View',
+    'Layout',
+    'Widget',
+    'Logger',
+    'FileManager',
+    'NetworkManager'
+]
