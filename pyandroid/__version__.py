7b4d0268ced6c97b58240fd26f8471d3dd0c5a02
Subhobhai <sarkarsubhadip604@gmail.com>
2025-11-10 12:27:35 +0530

    Bump version to 1.3.0 for PyPI release with latest docs.

--- pyandroid/__version__.py
+++ pyandroid/__version__.py
@@ -1,6 +1,6 @@
 """Version information for PyAndroid."""
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 __author__ = "Subhobhai"
 __license__ = "PyAndroid Custom License v1.0"
 __copyright__ = "Copyright 2025 Subhobhai"
